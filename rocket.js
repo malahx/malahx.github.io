796137a945aeec4043335e5394205b612095e006
Malah <malah@neuf.fr>
2016-10-24 16:45:51 +0200

    Passage de RocketJS en fonction annonyme

--- rocket.js
+++ rocket.js
@@ -2,117 +2,111 @@
 // Copyright 2016 Malah
 // This is free and unencumbered software released into the public domain.
 
-var RocketJS = {
-    currentGame: null,
-    show: function(toShow, toHide) {
-        toShow.classList.remove("hide");
-        for (var i = toHide.length -1; i>=0; i--) {
-            toHide[i].classList.add("hide");
-        }
-    },
+(function() {
+    var currentGame = null;
 
-    vector2: function(x, y) {
+    var vector2 = function(x, y) {
         this.x = x;
         this.y = y;
-    },
+    };
 
-    dim: function(obj, factor) {
-        this.obj = obj;
+    var Dimenions = function(dom, factor) {
+        this.dom = dom;
         this.factor = factor;
-        this.vect = new RocketJS.vector2(this.obj.width * factor, this.obj.height * factor);
+        this.vect = new vector2(this.dom.width * factor, this.dom.height * factor);
         this.x = this.vect.x;
         this.y = this.vect.y;
-        this.obj.setAttribute("width", this.vect.x + "px;");
-    },
+        this.dom.setAttribute("width", this.vect.x + "px;");
+    };
 
-    position: function(obj) {
-        this.obj = obj;
-        this.vect = new RocketJS.vector2((window.innerWidth - this.obj.x) / 2, window.innerHeight - this.obj.y);
+    var Positions = function(dim) {
+        this.dim = dim;
+        this.vect = new vector2((window.innerWidth - this.dim.x) / 2, window.innerHeight - this.dim.y);
         this.width = function(speed) {
             this.vect.x += speed;
-            this.obj.obj.style = "left: " + this.vect.x + "px;";
+            this.dim.dom.style = "left: " + this.vect.x + "px;";
         };
         this.height = function(speed) {
             this.vect.y += speed;
-            this.obj.obj.style = "top: " + this.vect.y + "px;";
+            this.dim.dom.style = "top: " + this.vect.y + "px;";
         };
         this.setWidth = function(posX) {
             this.vect.x = posX;
-            this.obj.obj.style = "left: " + this.vect.x + "px;";
+            this.dim.dom.style = "left: " + this.vect.x + "px;";
         };
         this.setHeight = function(posY) {
             this.vect.y = posY;
-            this.obj.obj.style = "top: " + this.vect.y + "px;";
+            this.dim.dom.style = "top: " + this.vect.y + "px;";
         };
         this.up = function() {
             return this.vect.y;
         };
         this.down = function() {
-            return this.vect.y + this.obj.y;
+            return this.vect.y + this.dim.y;
         };
         this.left = function() {
             return this.vect.x;
         }
         this.right = function() {
-            return this.vect.x + this.obj.x;
+            return this.vect.x + this.dim.x;
         }
         this.setWidth(this.vect.x);
         this.setHeight(this.vect.y);
-    },
+    };
 
-    Game: function() {
+    var Game = function() {
         this.rocket = document.createElement("img");
         this.fire = document.createElement("img");
         this.rocket.src = "img/fusee.png";
-        this.rocket.setAttribute("id","rocket");
-        this.rocket.setAttribute("position","fixed");
+        this.rocket.setAttribute("id", "rocket");
+        this.rocket.setAttribute("position", "fixed");
         this.fire.src = "img/fire.png";
-        this.fire.setAttribute("id","fire");
-        this.fire.setAttribute("position","fixed");
-        this.rocketDim = new RocketJS.dim(this.rocket, 0.5);
-        this.position = new RocketJS.position(this.rocketDim);
+        this.fire.setAttribute("id", "fire");
+        this.fire.setAttribute("position", "fixed");
+        game.appendChild(this.rocket);
+        this.rocketDim = new Dimenions(this.rocket, 0.5);
+        this.position = new Positions(this.rocketDim);
         this.speed = 0;
         this.accelerate = 0;
         this.burn = false;
-        game.appendChild(this.rocket);
         this.Update = function() {
-            var _cGame = RocketJS.currentGame;
-            setTimeout(_cGame.Update, 50);
-            if (_cGame.burn) {
-                _cGame.accelerate -= 0.5;
+            if (currentGame.burn) {
+                currentGame.accelerate -= 0.5;
+            }
+            currentGame.accelerate += 0.1;
+            if (currentGame.accelerate < -0.5) {
+                currentGame.accelerate = -0.5;
+            }
+            currentGame.speed += currentGame.accelerate;
+            if (currentGame.position.down() > window.innerHeight) {
+                currentGame.position.setHeight(window.innerHeight - currentGame.rocketDim.y)
+                currentGame.accelerate = 0;
+                currentGame.speed = 0;
             } else {
+                currentGame.position.height(currentGame.speed);
             }
-            _cGame.accelerate += 0.1;
-            if (_cGame.accelerate < -0.5) {
-                _cGame.accelerate = -0.5;
-            }
-            _cGame.speed += _cGame.accelerate;
-            if (_cGame.position.down() > window.innerHeight) {
-                _cGame.position.setHeight(window.innerHeight - _cGame.rocketDim.y)
-                _cGame.accelerate = 0;
-                _cGame.speed = 0;
-                return;
-            }
-            _cGame.position.height(_cGame.speed);
-        }
-        document.addEventListener("keydown", RocketJS.controlKeyPressDown);
-        document.addEventListener("keyup", RocketJS.controlKeyPressUp);
-        setTimeout(this.Update, 50);
-    },
+        };
+        document.addEventListener("keydown", controlKeyPressDown);
+        document.addEventListener("keyup", controlKeyPressUp);
+        setInterval(this.Update, 50);
+    };
 
-    controlKeyPressDown: function(evnt) {
+    var controlKeyPressDown = function(evnt) {
         var keyName = evnt.keyCode;
         if (keyName == 38) {
-            RocketJS.currentGame.burn = true;
-            console.log("Burn!");
+            currentGame.burn = true;
         }
-    },
+    };
 
-    controlKeyPressUp: function(evnt) {
+    var controlKeyPressUp = function(evnt) {
         var keyName = evnt.keyCode;
         if (keyName == 38) {
-            RocketJS.currentGame.burn = false;
-            console.log("Stop");
+            currentGame.burn = false;
         }
-    },
-};
+    };
+    window.addEventListener("load", function() {
+        rocketjs.addEventListener("click", function() {
+            currentGame = new Game();
+        });
+    });
+})();
